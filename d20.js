304f687b11b0cf99fabccb294b1aa8da761acc40
Zev <rakha2728@gmail.com>
2024-03-31 22:59:12 +0800

    Create d20.js

---
+++ d20.js
@@ -0,0 +1,9 @@
+// (8 kyu) Reversed sequence
+
+// Description:
+
+// Build a function that returns an array of integers from n to 1 where n>0.
+
+// Example : n=5 --> [5,4,3,2,1]
+
+const reverseSeq = (n) => [...Array(n)].map((el, i) => n - i);
