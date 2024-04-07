04607f8aeac228d980e59956675323fae9e42563
Zev <rakha2728@gmail.com>
2024-04-08 05:11:55 +0800

    Create d28.js

---
+++ d28.js
@@ -0,0 +1,9 @@
+// (8 kyu) Array plus array
+
+// Description:
+
+// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+
+// P.S. Each array includes only integer numbers. Output is a number too.
+
+const arrayPlusArray = (arr1, arr2) => [...arr1, ...arr2].reduce((acc, curr) => acc + curr);
