a77880223971a8c8ad1de5639ae8e1fdac70ee83
Zev <rakha2728@gmail.com>
2024-04-11 14:28:06 +0800

    Update d30.js

--- d30.js
+++ d30.js
@@ -1,15 +1,25 @@
-// (8 kyu) Grasshopper - Grade book
-
+// (8 kyu) Sum without highest and lowest number
 // Description:
-// Grade book
+// Task
 
-// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
 
-// Numerical Score 	    Letter Grade
-// 90 <= score <= 100 	'A'
-// 80 <= score < 90 	  'B'
-// 70 <= score < 80 	  'C'
-// 60 <= score < 70 	  'D'
-// 0 <= score < 60 	    'F'
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
 
-// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+// Mind the input validation.
+// Example
+
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+
+// Input validation
+
+// If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+const sumArray = (array) =>
+  array == null || array.length <= 2
+    ? 0
+    : array
+        .sort((a, b) => a - b)
+        .slice(1, -1)
+        .reduce((acc, curr) => acc + curr);
